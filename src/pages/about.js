b5c127a26a9739ed9cb959c9e2866779275da885
dillionmegida <dillionmegida@gmail.com>
2020-03-10 14:05:43 +0100

    Remove project section

--- src/pages/about.js
+++ src/pages/about.js
@@ -17,7 +17,7 @@ const ProjectList = [
         cover: 'https://res.cloudinary.com/dillionmegida/image/upload/v1570445060/images/website/burger-app-cover.jpg',
         link: 'https://deee-burger-app.herokuapp.com/'
     }
-    
+
 ];
 
 let About = () => (
@@ -28,88 +28,39 @@ let About = () => (
         PageKeywords='Dillion Megida, Dillion, Megida, web accessibility, DevCommunity, Front-end Developer, Technical Writer, Freelancer, Web Developer, Web Designer, Software Developer, Nigerian, Developer'
         TwitterCardTtitle='DILLION MEGIDA'
     >
-    	<h1 style={{textAlign: 'center'}}>About Me</h1>
-    	<section className={Styles.AboutSection}>
-    		<section className={Styles.Bio}>
-    			<h3 align='left'>Front End Developer, Technical Writer and Graphics Deeesigner</h3>
+        <h1 style={{ textAlign: 'center' }}>About Me</h1>
+        <section className={Styles.AboutSection}>
+            <section className={Styles.Bio}>
+                <h3 align='left'>Front End Developer, Technical Writer and Graphics Deeesigner</h3>
                 <p align='justify'>
-                    I'm passionate about Web Accessibility and JAMstack applications.<br/><br/>
-	                I write mostly about front-end web development topics on my blog here, <a href='https://blog.soshace.com/author/dillionmegida/' title='Soshace Blog'>Soshace</a> and <a href='https://dev.to/dillionmegida' title='DEV Blog'>Dev.to</a>. I'm also the Founder of <a href="https://thewebfor5.com" title="The Web For Five Homepage">TheWebFor5</a>.<br/><br/>
-                    Proficient in HTML, CSS, JS, ReactJS and GatsbyJS.<br/><br/>
-	                Efficient use of Adobe Photoshop and Adobe Illustrator for graphics designing. These include photo editing, logo designing and advertisement cards. <br/><br/>
-	                Average use of Adobe After Effects and Adobe Premiere Pro for video editing and motion graphics.
-                    <br/>
-	                Founder, <a href='https://twitter.com/deeesignsstudio'>Deeesigns Studios</a>, which deals in Graphics Designing.
-                    <br/>
-                    <br/>
+                    I'm passionate about Web Accessibility and JAMstack applications.<br /><br />
+                    I write mostly about front-end web development topics on my blog here, <a href='https://blog.soshace.com/author/dillionmegida/' title='Soshace Blog'>Soshace</a> and <a href='https://dev.to/dillionmegida' title='DEV Blog'>Dev.to</a>. I'm also the Founder of <a href="https://thewebfor5.com" title="The Web For Five Homepage">TheWebFor5</a>.<br /><br />
+                    Proficient in HTML, CSS, JS, ReactJS and GatsbyJS.<br /><br />
+                    Efficient use of Adobe Photoshop and Adobe Illustrator for graphics designing. These include photo editing, logo designing and advertisement cards. <br /><br />
+                    Average use of Adobe After Effects and Adobe Premiere Pro for video editing and motion graphics.
+                    <br />
+                    Founder, <a href='https://twitter.com/deeesignsstudio'>Deeesigns Studios</a>, which deals in Graphics Designing.
+                    <br />
+                    <br />
                     <a className={Styles.Resume} href='/resume.pdf' title='My Resume' target='_blank'>
                         Resume
                     </a>
                 </p>
-    		</section>
-    		<section className={Styles.Dp}>
-    			<Dp imgDivClass={Styles.ImgDiv}/>
-    			<h1>DILLION MEGIDA <span role='img' aria-label='Rocket Emoji'>&#128640;</span></h1>
+            </section>
+            <section className={Styles.Dp}>
+                <Dp imgDivClass={Styles.ImgDiv} />
+                <h1>DILLION MEGIDA <span role='img' aria-label='Rocket Emoji'>&#128640;</span></h1>
                 <p>
-                	<a className={Styles.Contact} href='mailto:dillionmegida@gmail.com'><i className="fa fa-envelope"></i> dillionmegida@gmail.com</a>
-	                <a className={Styles.Contact} href="tel:+2349058961095"><i className="fa fa-phone"></i> +234 905 896 1095</a>
+                    <a className={Styles.Contact} href='mailto:dillionmegida@gmail.com'><i className="fa fa-envelope"></i> dillionmegida@gmail.com</a>
+                    <a className={Styles.Contact} href="tel:+2349058961095"><i className="fa fa-phone"></i> +234 905 896 1095</a>
                     <Link to='/contact' title='Contact Me'>
                         Contact Me
                     </Link>
                 </p>
-    		</section>
-    	</section>
-
-        <section className={Styles.ProjectsSection}>
-            <h2>Projects I've Worked On</h2>
-            <p><b>Click/Hover For More Info</b></p>
-            <section className={Styles.Projects}>
-                {
-                    ProjectList.map((project, index) =>
-                        <div key={index}>
-                            <p className={Styles.ProjectTitle}>{project.title}</p>
-                            <p className={Styles.ProjectDesc}>
-                                {project.desc}
-                                <a href={project.link} title='View Project Live' target='_blank' rel='noopener noreferrer'>View Live</a>
-                            </p>
-                            <div className={Styles.ProjectCover}>
-                                <img src={project.cover} alt='Project Cover' />
-                            </div>
-                        </div>
-                    )
-                }
             </section>
-            <a className={Styles.CheckGitHub} href='https://github.com/dillionmegida' title='GitHub Profile'>Check More on GitHub</a>
         </section>
-
-    	{/* <section className={Styles.ServicesSection}>
-             <h2>What I do</h2>
-             <section className={Styles.Services}>
-                <section>
-                    <div>
-                        <img src={require('../img/web.png')} alt='Web Development Illustration'/>
-                        <h4>Web Development</h4>
-                    </div>
-                    <div>
-                        <img src={require('../img/brand.png')} alt='Branding Identity Illustration'/>
-                        <h4>Branding Identity</h4>
-                    </div>
-                </section>
-
-                <section>
-                    <div>
-                        <img src={require('../img/motion.png')} alt='Motion Graphics Illustration'/>
-                        <h4>Motion Graphics</h4>
-                    </div>
-                    <div>
-                        <img src={require('../img/flyer.png')} alt='Flyer and Banners Illustration'/>
-                        <h4>Flyers / Banners</h4>
-                    </div>
-                </section>
-
-            </section>
-        </section> */}
-    </Layout>
+        <a className={Styles.CheckGitHub} href='https://github.com/dillionmegida' title='GitHub Profile'>Check My Projects on GitHub</a>
+    </Layout >
 );
 
 export default About;
