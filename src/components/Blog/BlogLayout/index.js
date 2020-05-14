a3d3902504c0fd9f44e92daa55e2cc4353b73ec1
Dillion Megida <dillionmegida@gmail.com>
2020-05-14 20:05:58 +0100

    fix helmet props

--- src/components/Blog/BlogLayout/index.js
+++ src/components/Blog/BlogLayout/index.js
@@ -2,15 +2,11 @@ import React from "react"
 
 import Newsletter from "../../Newsletter"
 import Layout from "../../Layout"
-import WithLove from '../WithLove';
+import WithLove from "../WithLove"
 
 const BlogLayout = props => {
     return (
-        <Layout
-            PageTitle="Dillion Megida &#128640; - Technical Writer and Front End Developer"
-            PageLink="/"
-            PageDescription="Dillion is a front end developer, a technical writer and a graphics designer."
-        >
+        <Layout {...props}>
             {props.children}
             <WithLove />
             <Newsletter />
