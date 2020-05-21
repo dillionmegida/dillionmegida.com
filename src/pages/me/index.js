801c1404ea32e9512f5b9044e4ba2267b6202343
Dillion Megida <dillionmegida@gmail.com>
2020-05-21 03:14:59 +0100

    update header styles

--- src/pages/me/index.js
+++ src/pages/me/index.js
@@ -1,5 +1,5 @@
 import React from "react"
-import Styles from "./index.module.css"
+import Styles from "./index.module.scss"
 
 import Header from "../../components/Header"
 import AboutMe from "../../components/Me/About"
