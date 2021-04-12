5af7da3482ddae918f016e9d5bb8cd7cb66306b8
dillionmegida <dillionmegida@gmail.com>
2021-04-12 09:45:30 +0100

    feat: change newsletter signup to substack

--- src/components/Newsletter/index.js
+++ src/components/Newsletter/index.js
@@ -3,16 +3,6 @@ import Styles from "./index.module.scss"
 
 export default () => (
     <div className={Styles.Newsletter}>
-        <a
-            href="https://tinyletter.com/dillionmegida"
-            title="Subscribe to Newsletter"
-            target="_blank"
-            rel="noreferrer noopener"
-        >
-            Subscribe to Newsletter{" "}
-            <span role="img" aria-label="Newsletter emoji">
-                💌
-            </span>
-        </a>
+        <iframe src="https://dillionmegida.substack.com/embed" width="480" height="320"  frameborder="0" scrolling="no" />
     </div>
 )
