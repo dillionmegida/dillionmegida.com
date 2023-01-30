3ff13661f55bd16dafdd9cfb8a06e99a0aad0472
Dillion Megida <dillionmegida@gmail.com>
2023-01-30 09:59:08 +0100

    feat: add support for direct iframe slides

--- src/components/Talk/TalkFull/SlidesEmbed.tsx
+++ src/components/Talk/TalkFull/SlidesEmbed.tsx
@@ -1,6 +1,13 @@
 import React from "react"
 import styled from "styled-components"
 
+const DirectWrapper = styled.div`
+  iframe {
+    width: 100%;
+    height: calc(100vh - 100px);
+  }
+`
+
 const Iframe = styled.iframe`
   width: 100%;
   height: calc(100vh - 100px);
@@ -11,6 +18,11 @@ type Props = {
 }
 
 export default function SlidesEmbed({ url }: Props) {
+  if (url.startsWith("<iframe"))
+    return (
+      <DirectWrapper dangerouslySetInnerHTML={{ __html: url }}></DirectWrapper>
+    )
+
   return (
     <Iframe
       src={`${url}?start=false&loop=false&delayms=3000`}
